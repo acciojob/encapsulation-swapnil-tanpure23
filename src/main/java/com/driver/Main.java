5fe1c82fd21e8ed2293ba3ca270f5a03737a327e
Swapnil Tanpure <70815048+swapnil-tanpure23@users.noreply.github.com>
2023-05-10 12:39:08 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
   RWOnly obj = new RWOnly();
-  //obj.name = "John"; // Error: Cannot access private variable 'name'
+  //obj.name = "John"; // Error: Cannot access private string name
 //System.out.println(obj.name);
 obj.setName("John");
 System.out.println("Name: " + obj.getName());
